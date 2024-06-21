1223d55a48e992c2ea83c58a921e309c76161444
Ivan David Medina Vallez <iveen98@gmail.com>
2024-06-21 02:44:01 +0200

    Añadimos el patron Health

---
+++ Pacagroup.Ecommerce.Services.WebApi/Modules/HeallthCheck/HealthCheckCustom.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.Diagnostics.HealthChecks;
+
+namespace Pacagroup.Ecommerce.Services.WebApi.Modules.HeallthCheck
+{
+	public class HealthCheckCustom : IHealthCheck
+	{
+		private readonly Random _random = new Random();
+
+		public Task<HealthCheckResult> CheckHealthAsync(HealthCheckContext context, CancellationToken cancellationToken = default)
+		{
+			var responseTime = _random.Next(1, 300);
+
+			if (responseTime < 100) {
+
+				return Task.FromResult(HealthCheckResult.Healthy("Healthy resutl from HealthCheckCustom"));
+
+			}else if (responseTime < 200)
+			{
+				return Task.FromResult(HealthCheckResult.Degraded("Degreaded resutl from HealthCheckCustom"));
+			}
+
+			return Task.FromResult(HealthCheckResult.Unhealthy("Unhealthy resutl from HealthCheckCustom"));
+
+		}
+	}
+}
---
+++ Pacagroup.Ecommerce.Services.WebApi/Modules/HeallthCheck/HealthCheckExtensions.cs
@@ -0,0 +1,18 @@
+﻿namespace Pacagroup.Ecommerce.Services.WebApi.Modules.HeallthCheck
+{
+	public static class HealthCheckExtensions
+	{
+
+		public static IServiceCollection AddHealthCheck(this IServiceCollection services, IConfiguration configuration)
+		{
+			services.AddHealthChecks()
+				.AddSqlServer(configuration.GetConnectionString("NorthwindConnection"), tags: new[] { "databse" })
+				.AddCheck<HealthCheckCustom>("HealthCheckCustom", tags: new[] {"custom"});
+
+			services.AddHealthChecksUI()
+				.AddInMemoryStorage();
+
+			return services;
+		}
+	}
+}
